8c37293495d40b29bd296cdddad2df8ff103e856
= <=>
2024-05-06 09:58:43 -0300

    a

---
+++ src/Cliente.java
@@ -0,0 +1,21 @@
+import java.util.Date;
+public class Cliente {
+
+    protected String nome;
+    protected String endereco;
+    protected Date data;
+
+    public Cliente(String nome, String endereco, Date data) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.data = data;
+    }
+
+    public void imprimir(){System.out.println("cliente inválido");}
+
+    public String getNome() {return nome;}
+    public void setNome(String nome) {this.nome = nome;}
+//
+    public String getEndereco() {return endereco;}
+
+}
